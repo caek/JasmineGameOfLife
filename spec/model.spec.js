befc8b0213de592a4766e328e862786fc295efdc
Jo Cranford <jo.cranford@gmail.com>
2011-04-01 19:26:20 +0100

    Added test and wrote calculation

--- spec/model.spec.js
+++ spec/model.spec.js
@@ -4,9 +4,9 @@ describe("grid", function() {
 
         var expectedSecondGeneration = [
             [false, false, false, false, false, false, false, false],
-            [false, false, false, false, true, true, false, false],
-            [false, false, false, false, true, true, false, false],
-            [false, false, false, false, false, false, false, false]
+            [false, false, false, true, true, true, false, false],
+            [false, false, false, true, true, true, false, false],
+            [false, false, false, false, true, true, false, false]
 
         ]
 
@@ -27,4 +27,30 @@ describe("grid", function() {
 
     });
 
+    it("should die if it has more than three neighbours", function() {
+
+        var expectedSecondGeneration = [
+            [false, false, false, true, true, true, false, false],
+            [false, false, false, true, false, false, true, false],
+            [false, false, false, true, true, true, true, false],
+            [false, false, false, false, true, true, false, false]
+        ];
+
+        var grid = caek.GameOfLifeGrid(4, 8);
+
+        grid.setCurrentState([
+            [false, false, false, false, true, false, false, false],
+            [false, false, false, false, true, true, false, false],
+            [false, false, false, false, true, true, false, false],
+            [false, false, false, false, false, false, false, false]
+        ]);
+
+        grid.calculateNextGeneration();
+
+        var nextGeneration = grid.getCurrentState();
+
+        expect(nextGeneration).toEqual(expectedSecondGeneration);
+    });
+
 });
+
--- src/model.js
+++ src/model.js
@@ -1,19 +1,59 @@
 caek = {};
 
 caek.GameOfLifeGrid = function (){
-    return {
-        setCurrentState: function(){},
 
-        calculateNextGeneration: function(){},
+    var currentState;
+
+    return {
+        setCurrentState: function(state){
+            currentState = state;
+        },
+
+        calculateNextGeneration: function(){
+            var newState = [];
+            for (var i=0;i<currentState.length;i++) {
+                var row = currentState[i];
+                newRow = [];
+                for (var j=0;j<row.length;j++) {
+                    var countOfNeighbours = 0;
+
+                    var currentRow = currentState[i];
+                    if (currentRow[j-1]) countOfNeighbours++;
+                    if (currentRow[j+1]) countOfNeighbours++;
+
+                    var rowAbove = currentState[i-1];
+                    if(rowAbove) {
+                        if(rowAbove[j-1]) countOfNeighbours++;
+                        if(rowAbove[j]) countOfNeighbours++;
+                        if(rowAbove[j+1]) countOfNeighbours++;
+                    }
+                    var rowBelow = currentState[i+1];
+                    if(rowBelow) {
+                        if(rowBelow[j-1]) countOfNeighbours++;
+                        if(rowBelow[j]) countOfNeighbours++;
+                        if(rowBelow[j+1]) countOfNeighbours++;
+                    }
+
+                    if (countOfNeighbours > 3) {
+                        newRow.push(false);
+                    } else {
+                        if (countOfNeighbours < 2) {
+                            newRow.push(false);
+                        } else {
+                            newRow.push(true);
+                        }
+
+                    }
+//                    newRow.push(countOfNeighbours)
+                }
+                console.log(newRow)
+                newState.push(newRow);
+            }
+            currentState = newState;
+        },
 
         getCurrentState: function(){
-            return [
-                [false, false, false, false, false, false, false, false],
-                [false, false, false, false, true, true, false, false],
-                [false, false, false, false, true, true, false, false],
-                [false, false, false, false, false, false, false, false]
-
-            ]
+            return currentState;
         }
     };
 };
